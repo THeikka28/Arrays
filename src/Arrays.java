c93dab21a3dad3bd032079f80c6eee166608613e
THeikka28 <toby_heikka28@milton.edu>
2026-03-10 10:32:22 -0400

    added astro array that is randomly filled

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,4 +1,8 @@
 public class Arrays {
+    public String name;
+    public String lastname;
+
+
 //    make 3 methods
 
 //1. a method called averageNum that averages all the numers in the array and souts the result
@@ -7,10 +11,12 @@ public class Arrays {
 
 //3. a method called minNum that prints the smallest number in the array
     public int [] nums;
+    public Astronaut [] astronauts;
 
     public static void main(String[] args) {
         System.out.println("welcome to awesome arrays!");
         new Arrays();
+
 
     }
 
@@ -19,6 +25,7 @@ public class Arrays {
     public Arrays()
     {
         nums = new int[10];
+        astronauts = new Astronaut[10];
         nums[9] = 719;
         nums[1] = 12;
         nums[2] = 47;
@@ -34,8 +41,118 @@ public class Arrays {
         averageNum();
         maxnum();
         minnum();
+        for(int x = 0; x<astronauts.length; x++)
+        {
+            astronauts[x] = new Astronaut();
+        }
+
+        astrofill();
+        for(int x = 0; x<astronauts.length; x++)
+        {
+            astronauts[x].printinfo();
+        }
 
     }
+
+    public void astrofill ()
+    {
+        for (int x = 0; x<astronauts.length; x++)
+        {
+            int rand = (int) (Math.random()*2)+1;
+            int rand1 = (int) (Math.random()*10)+1;
+
+
+            astronauts[x].eyes = rand;
+            if(rand1 == 1)
+            {
+                name = "Sharon";
+            }
+            if(rand1 == 2)
+             {
+            name = "John";
+            }
+            if(rand1 == 3)
+            {
+                name = "Bob";
+            }
+            if(rand1 == 4)
+            {
+                name = "Billy";
+            }
+            if(rand1 == 5)
+            {
+                name = "Shakira";
+            }
+            if(rand1 == 6)
+            {
+                name = "Adelle";
+            }
+            if(rand1 == 7)
+            {
+                name = "Billy Bob Joel";
+            }
+            if(rand1 == 8)
+            {
+                name = "Kimmy";
+            }
+            if(rand1 == 9)
+            {
+                name = "Timothee";
+            }
+            if(rand1 == 10)
+            {
+                name = "Michael";
+            }
+
+
+            rand1 = (int) (Math.random()*10)+1;
+
+
+            astronauts[x].eyes = rand;
+            if(rand1 == 1)
+            {
+                lastname = "Sharon";
+            }
+            if(rand1 == 2)
+            {
+                lastname = "John";
+            }
+            if(rand1 == 3)
+            {
+                lastname = "Bob";
+            }
+            if(rand1 == 4)
+            {
+                lastname = "Billy";
+            }
+            if(rand1 == 5)
+            {
+                lastname = "Shakira";
+            }
+            if(rand1 == 6)
+            {
+                lastname = "Adelle";
+            }
+            if(rand1 == 7)
+            {
+                lastname = "Billy Bob Joel";
+            }
+            if(rand1 == 8)
+            {
+                lastname = "Kimmy";
+            }
+            if(rand1 == 9)
+            {
+                lastname = "Timothee";
+            }
+            if(rand1 == 10)
+            {
+                lastname = "Michael";
+            }
+            astronauts[x].name = name + " "+  lastname;
+        }
+    }
+
     public void arrayfill()
     {
         for (int x =0; x<nums.length;x++)
@@ -75,7 +192,7 @@ public class Arrays {
     public void maxnum()
     {
         int biggest;
-        biggest = 0;
+        biggest = nums[0];
         for (int i = 0; i < nums.length; i++)
         {
             if(nums[i]>biggest)
@@ -89,7 +206,7 @@ public class Arrays {
     public void minnum()
     {
         int smallest;
-        smallest = 101;
+        smallest = nums[0];
         for (int i = 0; i < nums.length; i++)
         {
             if(nums[i]< smallest)
---
+++ src/Astronaut.java
@@ -0,0 +1,14 @@
+public class Astronaut {
+    public String name;
+    public int eyes;
+
+    public Astronaut()
+    {
+
+    }
+    public void printinfo()
+    {
+        System.out.println( "The astronaut has " + eyes + " eyes and is named " +  name);
+    }
+
+}
